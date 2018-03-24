440ec17e942368c9e34c574348f7ab87de9bf93d
Kirill Lebedev <echo.bjornd@gmail.com>
2018-03-24 20:50:39 +0200

    Java cbl module

---
+++ android/src/main/java/com/reactlibrary/ConversionUtil.java
@@ -0,0 +1,215 @@
+/**
+ *  ConversionUtil.java
+ *  react-native-eval
+ *
+ *  Created by Andy Prock on 9/24/15.
+ */
+
+package com.reactlibrary;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+/**
+ * Helper utilities to convert from react-native types to POJOs
+ */
+/* package */ final class ConversionUtil {
+    /**
+     * toObject extracts a value from a {@link ReadableMap} by its key,
+     * and returns a POJO representing that object.
+     *
+     * @param readableMap The Map to containing the value to be converted
+     * @param key The key for the value to be converted
+     * @return The converted POJO
+     */
+    public static Object toObject(@Nullable ReadableMap readableMap, String key) {
+        if (readableMap == null) {
+            return null;
+        }
+
+        Object result;
+
+        ReadableType readableType = readableMap.getType(key);
+        switch (readableType) {
+            case Null:
+                result = null;
+                break;
+            case Boolean:
+                result = readableMap.getBoolean(key);
+                break;
+            case Number:
+                // Can be int or double.
+                double tmp = readableMap.getDouble(key);
+                if (tmp == (int) tmp) {
+                    result = (int) tmp;
+                } else {
+                    result = tmp;
+                }
+                break;
+            case String:
+                result = readableMap.getString(key);
+                break;
+            case Map:
+                result = toMap(readableMap.getMap(key));
+                break;
+            case Array:
+                result = toList(readableMap.getArray(key));
+                break;
+            default:
+                throw new IllegalArgumentException("Could not convert object with key: " + key + ".");
+        }
+
+        return result;
+    }
+
+    /**
+     * toMap converts a {@link ReadableMap} into a HashMap.
+     *
+     * @param readableMap The ReadableMap to be conveted.
+     * @return A HashMap containing the data that was in the ReadableMap.
+     */
+    public static Map<String, Object> toMap(@Nullable ReadableMap readableMap) {
+        if (readableMap == null) {
+            return null;
+        }
+
+        com.facebook.react.bridge.ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+        if (!iterator.hasNextKey()) {
+            return null;
+        }
+
+        Map<String, Object> result = new HashMap<>();
+        while (iterator.hasNextKey()) {
+            String key = iterator.nextKey();
+            result.put(key, toObject(readableMap, key));
+        }
+
+        return result;
+    }
+
+    /**
+     * toList converts a {@link ReadableArray} into an ArrayList.
+     *
+     * @param readableArray The ReadableArray to be conveted.
+     * @return An ArrayList containing the data that was in the ReadableArray.
+     */
+    public static List<Object> toList(@Nullable ReadableArray readableArray) {
+        if (readableArray == null) {
+            return null;
+        }
+
+        List<Object> result = new ArrayList<>(readableArray.size());
+        for (int index = 0; index < readableArray.size(); index++) {
+            ReadableType readableType = readableArray.getType(index);
+            switch (readableType) {
+                case Null:
+                    result.add(String.valueOf(index));
+                    break;
+                case Boolean:
+                    result.add(readableArray.getBoolean(index));
+                    break;
+                case Number:
+                    // Can be int or double.
+                    double tmp = readableArray.getDouble(index);
+                    if (tmp == (int) tmp) {
+                        result.add((int) tmp);
+                    } else {
+                        result.add(tmp);
+                    }
+                    break;
+                case String:
+                    result.add(readableArray.getString(index));
+                    break;
+                case Map:
+                    result.add(toMap(readableArray.getMap(index)));
+                    break;
+                case Array:
+                    result = toList(readableArray.getArray(index));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Could not convert object with index: " + index + ".");
+            }
+        }
+
+        return result;
+    }
+
+    public static WritableArray toWritableArray(Object[] array) {
+        WritableArray writableArray = Arguments.createArray();
+
+        for (int i = 0; i < array.length; i++) {
+            Object value = array[i];
+
+            if (value == null) {
+                writableArray.pushNull();
+            }
+            if (value instanceof Boolean) {
+                writableArray.pushBoolean((Boolean) value);
+            }
+            if (value instanceof Double) {
+                writableArray.pushDouble((Double) value);
+            }
+            if (value instanceof Integer) {
+                writableArray.pushInt((Integer) value);
+            }
+            if (value instanceof String) {
+                writableArray.pushString((String) value);
+            }
+            if (value instanceof Map) {
+                writableArray.pushMap(ConversionUtil.toWritableMap((Map<String, Object>) value));
+            }
+            if (value.getClass().isArray()) {
+                writableArray.pushArray(ConversionUtil.toWritableArray((Object[]) value));
+            }
+            if (value instanceof List) {
+                writableArray.pushArray(ConversionUtil.toWritableArray(((List) value).toArray()));
+            }
+        }
+
+        return writableArray;
+    }
+
+    public static WritableMap toWritableMap(Map<String, Object> map) {
+        WritableMap writableMap = Arguments.createMap();
+        Iterator iterator = map.entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry pair = (Map.Entry)iterator.next();
+            Object value = pair.getValue();
+
+            if (value == null) {
+                writableMap.putNull((String) pair.getKey());
+            } else if (value instanceof Boolean) {
+                writableMap.putBoolean((String) pair.getKey(), (Boolean) value);
+            } else if (value instanceof Double) {
+                writableMap.putDouble((String) pair.getKey(), (Double) value);
+            } else if (value instanceof Integer) {
+                writableMap.putInt((String) pair.getKey(), (Integer) value);
+            } else if (value instanceof String) {
+                writableMap.putString((String) pair.getKey(), (String) value);
+            } else if (value instanceof Map) {
+                writableMap.putMap((String) pair.getKey(), ConversionUtil.toWritableMap((Map<String, Object>) value));
+            } else if (value.getClass() != null && value.getClass().isArray()) {
+                writableMap.putArray((String) pair.getKey(), ConversionUtil.toWritableArray((Object[]) value));
+            } else if (value instanceof List) {
+                writableMap.putArray((String) pair.getKey(), ConversionUtil.toWritableArray(((List) value).toArray()) );
+            }
+
+            iterator.remove();
+        }
+
+        return writableMap;
+    }
+}
--- android/src/main/java/com/reactlibrary/RNReactNativeCblModule.java
+++ android/src/main/java/com/reactlibrary/RNReactNativeCblModule.java
@@ -1,22 +1,326 @@
 
 package com.reactlibrary;
 
+import android.text.TextUtils;
+
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.DocumentChange;
+import com.couchbase.lite.LiveQuery;
+import com.couchbase.lite.Mapper;
+import com.couchbase.lite.Query;
+import com.couchbase.lite.QueryEnumerator;
+import com.couchbase.lite.QueryRow;
+import com.couchbase.lite.Reducer;
+import com.couchbase.lite.View;
+import com.couchbase.lite.android.AndroidContext;
+import com.couchbase.lite.auth.Authenticator;
+import com.couchbase.lite.auth.AuthenticatorFactory;
+import com.couchbase.lite.replicator.Replication;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableMap;
 
-public class RNReactNativeCblModule extends ReactContextBaseJavaModule {
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.util.ZipUtils;
+import com.couchbase.lite.javascript.JavaScriptViewCompiler;
+import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
-  private final ReactApplicationContext reactContext;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.IOException;
+import java.util.UUID;
 
-  public RNReactNativeCblModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+import javax.annotation.Nullable;
+
+public class RNReactNativeCblModule extends ReactContextBaseJavaModule implements Database.ChangeListener {
+
+  private final ReactContext mReactContext;
+  private Database db = null;
+  private final HashMap<String, Document> liveDocuments = new HashMap<>();
+  private final HashMap<String, LiveQuery> liveQueries = new HashMap<>();
 
   @Override
   public String getName() {
-    return "RNReactNativeCbl";
+    return "CouchbaseLite";
+  }
+
+  public RNReactNativeCblModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    mReactContext = reactContext;
+  }
+
+  private void sendEvent(String eventName, @Nullable WritableMap params) {
+    mReactContext
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit(eventName, params);
+  }
+
+  @ReactMethod
+  public void openDb(String name, Boolean installPrebuildDb, Promise promise) {
+    if (this.db == null) {
+      try {
+        View.setCompiler(new JavaScriptViewCompiler());
+        Manager manager = new Manager(new AndroidContext(mReactContext.getApplicationContext()), Manager.DEFAULT_OPTIONS);
+        if (installPrebuildDb) {
+          Database db = manager.getExistingDatabase(name);
+          if (db == null) {
+            ZipUtils.unzip(this.mReactContext.getAssets().open(name + ".zip"), manager.getContext().getFilesDir());
+          }
+        }
+        this.db = manager.getDatabase(name);
+        this.db.addChangeListener(this);
+        promise.resolve(null);
+      } catch (IOException | CouchbaseLiteException e) {
+        promise.reject("open_database", "Can not open database", e);
+      }
+    } else {
+      promise.resolve(null);
+    }
+  }
+
+  @ReactMethod
+  public void getDocument(String docId, Promise promise) {
+    Document doc = this.db.getExistingDocument(docId);
+    if (doc == null) {
+      promise.reject("update_document", "Can not find document");
+    } else {
+      promise.resolve( ConversionUtil.toWritableMap( new HashMap<String, Object>(doc.getProperties())) );
+    }
+  }
+
+  @ReactMethod
+  public void createDocument(ReadableMap properties, Promise promise) {
+    Document doc = this.db.createDocument();
+    Map<String, Object> props = new HashMap<>();
+    props.putAll(properties.toHashMap());
+    try {
+      doc.putProperties(props);
+      promise.resolve(doc.getId());
+    } catch (CouchbaseLiteException e) {
+      promise.reject("create_document", "Can not create document", e);
+    }
+  }
+
+  @ReactMethod
+  public void updateDocument(String docId, ReadableMap properties, Promise promise) {
+    Document doc = this.db.getDocument(docId);
+    if (doc == null) {
+      promise.reject("update_document", "Can not find document");
+      return;
+    }
+    Map<String, Object> props = new HashMap<>();
+    if (doc.getCurrentRevision() != null) {
+      props.putAll(doc.getProperties());
+    }
+    props.putAll(properties.toHashMap());
+    try {
+      doc.putProperties(props);
+      promise.resolve(null);
+    } catch (CouchbaseLiteException e) {
+      promise.reject("update_document", "Can not update document", e);
+    }
+  }
+
+  @ReactMethod
+  public void deleteDocument(String docId, Promise promise) {
+    Document doc = this.db.getDocument(docId);
+    try {
+      doc.delete();
+      promise.resolve(null);
+    } catch (CouchbaseLiteException e) {
+      promise.reject("delete_document", "Can not delete document", e);
+    }
+  }
+
+  @ReactMethod
+  public void createLiveDocument(String docId, Promise promise) {
+    Document doc = this.db.getDocument(docId);
+    final String uuid = UUID.randomUUID().toString();
+    final RNReactNativeCblModule self = this;
+    doc.addChangeListener(new Document.ChangeListener() {
+      @Override
+      public void changed(Document.ChangeEvent event) {
+        DocumentChange docChange = event.getChange();
+        WritableMap params = Arguments.createMap();
+        params.putString("uuid", uuid);
+        Map<String, Object> props = new HashMap<>(docChange.getAddedRevision().getProperties());
+        params.putMap("data", ConversionUtil.toWritableMap(props));
+        self.sendEvent("liveDocumentChange", params);
+      }
+    });
+    this.liveDocuments.put(uuid, doc);
+    promise.resolve(uuid);
+    WritableMap params = Arguments.createMap();
+    params.putString("uuid", uuid);
+    Map<String, Object> props = new HashMap<>(doc.getProperties());
+    params.putMap("data", ConversionUtil.toWritableMap(props));
+    this.sendEvent("liveDocumentChange", params);
+  }
+
+  @ReactMethod
+  public void destroyLiveDocument(String uuid, Promise promise) {
+    this.liveDocuments.remove(uuid);
+    promise.resolve(null);
+  }
+
+  private View getView(String view) {
+    View dbview = this.db.getView(view);
+    if (dbview.getMap() == null) {
+      String[] path = view.split("/");
+      Document ddoc = this.db.getDocument("_design/" + path[0]);
+      String viewName = TextUtils.join("/", Arrays.copyOfRange(path, 1, path.length) );
+      Map<String, Object> container = (Map<String, Object>) ddoc.getProperty("views");
+      Map<String, String> viewDesc = (Map<String, String>)container.get(viewName);
+      String mapString = viewDesc.get("map");
+      String reduceString = viewDesc.get("reduce");
+      Mapper mapBlock = View.getCompiler().compileMap(mapString, "javascript");
+      Reducer reduceBlock = null;
+      if (reduceString != null) {
+        reduceBlock = View.getCompiler().compileReduce(reduceString, "javascript");
+      }
+      dbview.setMapReduce(mapBlock, reduceBlock, "1");
+    }
+    return dbview;
+  }
+
+  private void setQueryParams(Query query, ReadableMap params) {
+    if (params == null) {
+      return;
+    }
+    if (params.hasKey("groupLevel")) {
+      query.setGroupLevel( params.getInt("groupLevel") );
+    }
+    if (params.hasKey("keys")) {
+      query.setKeys( params.getArray("keys").toArrayList() );
+    }
+    if (params.hasKey("startKey")) {
+      if (params.getType("startKey") == ReadableType.Array) {
+        query.setStartKey( params.getArray("startKey").toArrayList() );
+      } else {
+        query.setStartKey( params.getString("startKey") );
+      }
+    }
+    if (params.hasKey("endKey")) {
+      if (params.getType("endKey") == ReadableType.Array) {
+        query.setEndKey( params.getArray("endKey").toArrayList() );
+      } else {
+        query.setEndKey( params.getString("endKey") );
+      }
+    }
+    if (params.hasKey("descending")) {
+      query.setDescending( params.getBoolean("descending") );
+    }
+    if (params.hasKey("limit")) {
+      query.setLimit( params.getInt("limit") );
+    }
+  }
+
+  @ReactMethod
+  public void query(String view, ReadableMap params, Promise promise) {
+    View dbview = this.getView(view);
+    Query query = dbview.createQuery();
+    try {
+      QueryEnumerator result = query.run();
+      promise.resolve( ConversionUtil.toWritableArray( this.getQueryResults(result).toArray() ) );
+    } catch (CouchbaseLiteException e) {
+      promise.reject("query", "Error running query", e);
+    }
+  }
+
+  @ReactMethod
+  public void createLiveQuery(String view, ReadableMap params, Promise promise) {
+    View dbview = this.getView(view);
+    Query query = dbview.createQuery();
+    this.setQueryParams(query, params);
+    final LiveQuery liveQuery = query.toLiveQuery();
+    final String uuid = UUID.randomUUID().toString();
+    final RNReactNativeCblModule self = this;
+    liveQuery.addChangeListener(new LiveQuery.ChangeListener() {
+      @Override
+      public void changed(LiveQuery.ChangeEvent event) {
+        WritableMap params = Arguments.createMap();
+        params.putString("uuid", uuid);
+        params.putArray("data", ConversionUtil.toWritableArray( self.getQueryResults(event.getRows()).toArray() ));
+        self.sendEvent("liveQueryChange", params);
+      }
+    });
+    this.liveQueries.put(uuid, liveQuery);
+    promise.resolve(uuid);
+    liveQuery.start();
+  }
+
+  @ReactMethod
+  public void destroyLiveQuery(String uuid, Promise promise) {
+    this.liveQueries.remove(uuid);
+    promise.resolve(null);
+  }
+
+  public void changed(Database.ChangeEvent event) {
+    for (DocumentChange change : event.getChanges()) {
+      for (String queryUuid : this.liveQueries.keySet()) {
+        QueryEnumerator rows = this.liveQueries.get(queryUuid).getRows();
+        while (rows.hasNext()) {
+          QueryRow row = rows.next();
+          if (row.getDocumentId().equals(change.getDocumentId())) {
+            WritableMap params = Arguments.createMap();
+            params.putString("uuid", queryUuid);
+            params.putArray("data", ConversionUtil.toWritableArray(this.getQueryResults(rows).toArray()));
+            this.sendEvent("liveQueryChange", params);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  private ArrayList getQueryResults(QueryEnumerator result) {
+    ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+    for (Iterator<QueryRow> it = result; it.hasNext(); ) {
+      QueryRow row = it.next();
+      Document doc = row.getDocument();
+      if (doc != null) {
+        HashMap<String, Object> props = new HashMap(doc.getProperties());
+        if (props != null) {
+          list.add( props );
+        }
+      } else {
+        HashMap<String, Object> item = new HashMap();
+        item.put("key", row.getKey());
+        item.put("value", row.getValue());
+        list.add(item);
+      }
+    }
+    return list;
+  }
+
+  @ReactMethod
+  public void startReplication(String remoteUrl, String facebookToken, Promise promise) {
+    try {
+      URL url = new URL(remoteUrl);
+      Replication push = this.db.createPushReplication(url);
+      Replication pull = this.db.createPullReplication(url);
+      pull.setContinuous(true);
+      push.setContinuous(true);
+      Authenticator auth = AuthenticatorFactory.createFacebookAuthenticator(facebookToken);
+      push.setAuthenticator(auth);
+      pull.setAuthenticator(auth);
+      promise.resolve(null);
+    } catch (MalformedURLException e) {
+      promise.reject("start_replication", "Malformed remote URL", e);
+    }
+
   }
 }
